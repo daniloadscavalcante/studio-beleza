78a17168a3540f194b3129a6ff9a9aabffeae08e
Danilo <daniloadscavalcante@gmail.com>
2023-06-02 09:21:14 -0300

    inclusão de interatividade na galeria, inclusão de imagens,pagina submenu

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,87 +1,49 @@
 import { useEffect, useState } from "react"
 
 import "./Galeria.css"
+import HeroBase from "../components/heroBase/HeroBase"
 
 export default function Galeria(){
 
     const [galeria, setGaleria] = useState([])
-    const [novosdados, setnovosdados] = useState([])
+    const [file, setFile] = useState(null)
 
+    
+    
     useEffect(() =>{
         fetch ('./dados/galeria.json')
         .then((r)=> r.json())
         .then((dados)=>{
-            setGaleria(dados)
-            console.log(dados[0].url)
-            
+            setGaleria(dados)          
         })
         .catch((error) => console.log("não pegou os dados")) 
-    
-        fetch ('./dados/test.json')
-            .then((r)=> r.json())
-            .then((dados)=>{
-                console.log(dados.galeria);
-                setnovosdados(dados.galeria)
-            })
-            .catch((error) => console.log("não pegou os dados test")) 
-
     }, [])
 
+ 
 
     return (
     
     <main>
+        <HeroBase 
+        titulo="galeria"
+        bg="/img/banner-geral.jpg"
+        />
         <p>Pagina Galeria</p>
-        <section className="introducao">            
-           <h2>Conheça um pouco do<br/> nosso trabalho</h2>          
-        </section>         
+        <div>            
+           <h2 className="title-center">Conheça um pouco do<br/> nosso trabalho</h2>          
+        </div>         
         <section className="galeria"> 
         {galeria.map((item, index) => <div key={index} className={item.style}>
-                <img src={item.url} alt={item.alt}/>
-            </div> )}
+                <img src={item.url} alt={item.alt} onClick={() => setFile(item)}/>
+            </div> )     
+            }
         </section>
-        {novosdados.map((novosdado, index) => <div key={index}><span>{novosdado.src}</span></div>)}
-        {/* {galeria.length > 0 ? <section className="galeria">  
-            <div className="galeriaImg1 w-3 h-2" >
-                <img src={galeria[0].url} alt={galeria[0].alt}/>
-            </div> 
-            <div className="galeriaImg2 w-2 h-3" >
-                <img src={galeria[1].url} alt={galeria[1].alt}/>
-            </div>
-            <div className="galeriaImg3 w-3 h-2"> 
-                <img src={galeria[2].url} alt={galeria[2].alt}/>
-            </div>	
-	        <div className="galeriaImg4 w-3 h-2">
-                <img src={galeria[3].url} alt={galeria[3].alt}/>
-            </div>
-            <div className="galeriaImg5 w-3 h-2" >
-                <img src={galeria[4].url} alt={galeria[4].alt}/>
-            </div>
-            <div className="galeriaImg6 w-3 h-2">
-                <img src={galeria[5].url} alt={galeria[5].alt}/>
-            </div>
-            <div className="galeriaImg7 w-2 h-3" >
-                <img src={galeria[6].url} alt={galeria[6].alt}/>
-            </div>
-            <div className="galeriaImg8 w-4 h-2">
-                <img src={galeria[7].url} alt={galeria[7].alt}/>
-            </div>
-            <div className="galeriaImg9 w-3 h-3" >
-                <img src={galeria[8].url} alt={galeria[8].alt}/>
-            </div>
-            <div className="galeriaImg10 w-4 h-2" >
-                <img src={galeria[9].url} alt={galeria[9].alt}/>
-            </div>
-            <div className="galeriaImg11 w-3 h-2" >
-                <img src={galeria[10].url} alt={galeria[10].alt}/>
-            </div>
-            <div className="galeriaImg12 w-3 h-2" >
-                <img src={galeria[11].url} alt={galeria[11].alt}/>
-            </div>
-            
-            
-        </section> : null} */}
-                     
+
+            {file ? <div className="lightbox" style={{display: file ? "flex" : "none"}}>
+                <span onClick={() => setFile(null)}>&times;</span><img src={file.url} alt={file.alt}/>
+            </div> : null}
+        
+                             
     </main>
     
     )
--- src/pages/subMenu/Maquiagem.jsx
+++ src/pages/subMenu/Maquiagem.jsx
@@ -1,3 +1,60 @@
+import img1 from '../../assets/imagensMaquiagem.png'
+import img2 from '../../assets/maquiagemInlustrativa.png'
+import img3 from '../../assets/FotoMaquiagem.jpg'
+import icone from '../../assets/icone-pincelMaq.png'
+import HeroBase from '../../components/heroBase/HeroBase'
+
 export default function Maquiagem(){
-    return <p>pagina Maquiagem</p>
+    return (
+    
+    <main>
+        <HeroBase 
+        titulo="maquiagem"
+        bg="/img/banner-maquiagem.jpg"
+        />
+        <p>pagina Maquiagem</p>
+    <section className="maquiagem">
+        <div className="iconePincel">
+            <img src={icone} alt="icone pincel" styles="width: 100px;"/>
+        </div>
+        
+       <h2>Faça maquiagens poderosas</h2>
+       <div className="capaMaquiagem">
+            <img src={img1} alt=""/>
+       </div>
+    </section>      
+    
+    <section class="maquiagem-servico">
+        <div class="maquiagem-servico__center">                
+            <div className="servico maquiagem-servico--coluna">                    
+                <ul className="servico__lista servico__lista--coluna servico__lista--maquiagem">                        
+                    <h4 className="servico__titulo servico__titulo--estetica">Serviços:</h4>
+                    <li>fi a fio<i className="fa-solid fa-check"></i></li>
+                    <li>lash lifting<i className="fa-solid fa-check"></i></li>
+                    <li>volume brasileiro<i className="fa-solid fa-check"></i></li>
+                    <li>classico<i className="fa-solid fa-check"></i></li>
+                    <a href="#" className="btn servico__btn--maquiagem">Saiba mais</a>
+                </ul>
+                <div className="servico__imgMaquiagens">
+                    <img src={img2} alt="imagem de maquiagens"/>
+                </div>
+            </div>                
+        </div>
+    </section>
+
+    <section className="maquiagem-info">
+        <div className="maquiagem-info__img">
+            <img src={img3} alt=""/>
+        </div>
+       <div className="maquiagem-info__texto">                
+            <h4>Micropigmentação:</h4>
+            <p>é um procedimento diferente da antiga maquiagem definitiva, porque é, na verdade, uma maquiagem temporária. O aparelho usado tem uma rotação menor que o da tatuagem, deixando o traço mais suave.O pigmento usado é próprio para o rosto e não sofre variação de cor como o da tatuagem, que após alguns anos azulava as sobrancelhas.</p>
+            <h4>Maquiagem definitiva:</h4>
+            <p>é um tipo de tatuagem que perfura a camada mais profunda da pele. Se o resultado não agradar, é complicado livrar-se do problema.</p>                  
+        </div>
+    </section>        
+</main>
+    
+    
+    )
 }
